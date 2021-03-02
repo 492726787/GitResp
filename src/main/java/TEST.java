0a7c8b834fadb3d81afd17ca8c4e3625d1ea0a9f
wzh <492726787@qq.com>
2021-03-02 19:51:58 +0800

    第一次提交Test测试类

---
+++ src/main/java/TEST.java
@@ -0,0 +1,12 @@
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import java.security.PublicKey;
+
+/**
+ * @author shkstart
+ * @create 2021-03-02 19:49
+ */
+public class TEST {
+    private int cellphone;
+    public String name;
+}
